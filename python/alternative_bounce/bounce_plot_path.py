01f690b52c15ea179c9ce0d571d6bef8e83e83a6
Alex Levin <alevin@gmail.com>
2021-03-28 15:09:12 -0700

    alternative bounce model

---
+++ python/alternative_bounce/bounce_plot_path.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import argparse
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.interpolate as ip
+import easygui
+
+
+abspath = os.path.abspath(__file__)
+os.chdir(os.path.dirname(abspath))
+
+image_display_time = 3000
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-n", "--name", help="SeriesName")
+args = parser.parse_args()
+
+def close_event():
+    plt.close()
+
+def draw_line(data_file,line_color):
+    data = pd.read_csv(data_file)
+    x = data['X']
+    y = data['Y']
+    point = data['point']
+    pointi = np.linspace(1, len(data), 100)
+
+    xi = ip.CubicSpline(point, x, bc_type='clamped')
+    yi = ip.CubicSpline(point, y, bc_type='clamped')
+
+    ax.scatter(xm, ym, color='r', marker='*', linewidth=5, alpha=1)
+    ax.plot(xi(pointi), yi(pointi), color=line_color, linewidth=5, alpha=0.5)
+    ax.scatter(x, y, color='b', alpha=0.5)
+    ax.plot(x, y, color='b', alpha=0.2)
+    for i, txt in enumerate(point):
+        plt.annotate(txt, (x[i], y[i]))
+
+
+
+markers_file = 'bounce_markers.csv'
+data_file0= args.name + '_0.csv'
+data_file1= args.name + '_1.csv'
+data_file2= args.name + '_2.csv'
+data_file3= args.name + '_3.csv'
+
+out_file = args.name + '.png'
+
+fig = plt.figure(dpi=120)  # facecolor='w', edgecolor='k')
+timer = fig.canvas.new_timer(interval=image_display_time)
+timer.add_callback(close_event)
+
+ax = fig.add_subplot(1, 1, 1)
+
+x_major_ticks = np.arange(0, 361, 30)
+y_major_ticks = np.arange(-180, 1, 30)
+ax.set_xticks(x_major_ticks)
+ax.set_yticks(y_major_ticks)
+
+ax.grid(which='both')
+ax.set_xlim(0, 360)
+ax.set_ylim(-180, 0)
+ax.set_aspect(1)
+
+markers = pd.read_csv(markers_file)
+xm = markers['X']
+ym = markers['Y']
+
+#data = pd.read_csv(data_file0)
+#x = data['X']
+#y = data['Y']
+#point = data['point']
+#pointi = np.linspace(1, len(data), 100)
+#
+#xi = ip.CubicSpline(point, x, bc_type='clamped')
+#yi = ip.CubicSpline(point, y, bc_type='clamped')
+#
+#ax.scatter(xm, ym, color='r', marker='*', linewidth=5, alpha=1)
+#ax.plot(xi(pointi), yi(pointi), color='g', linewidth=5, alpha=0.5)
+#ax.scatter(x, y, color='b', alpha=0.5)
+#ax.plot(x, y, color='b', alpha=0.2)
+#for i, txt in enumerate(point):
+#    plt.annotate(txt, (x[i], y[i]))
+#
+
+draw_line(data_file0, 'g')
+draw_line(data_file1, 'c')
+draw_line(data_file2, 'y')
+draw_line(data_file3, 'm')
+
+plt.savefig(out_file,bbox_inches='tight')
+
+timer.start()
+plt.show()
+print('result is saved as ' + out_file)
+
